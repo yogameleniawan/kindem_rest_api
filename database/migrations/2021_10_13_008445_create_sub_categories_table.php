6d5755606ee539a3327e39d43f6b6f2af98633e4
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-10-14 12:23:26 +0700

    update sub categories

---
+++ database/migrations/2021_10_13_008445_create_sub_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->char('id', 10)->primary();
+            $table->string('name');
+            $table->string('image');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('RESTRICT')->onUpdate('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
