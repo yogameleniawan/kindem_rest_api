66c70c203dd1c392a7e4d614371eb19da73db358
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-10-27 22:09:30 +0700

    add api auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 });
