dc22160a5c688e2a7a198cbd7d5539d5699c5756
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-10-27 11:30:42 +0700

    update auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
-            $table->string('role');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::prefix('admin')->group(function () {
         'user_details' => UserDetailsController::class,
     ]);
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
