f3cfb122f8f480e4b90cd5eb811f69c69e56c847
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-11-13 17:30:12 +0700

    update login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,135 +1,85 @@
-<style>
-    .square1 {
-        float: right;
-        background-color: #007251;
-        width: 500px;
-        height: 100%;
-        padding: 27px;
-    }
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Stulish | Login</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    .input[type=email],
-    input[type=password] {
-        width: 100%;
-        margin-top: 7px;
-        margin-bottom: 7px;
-    }
+        <link rel="icon" href="../favicon.ico" type="image/x-icon" />
 
-    .btn-login {
-        background-color: #fff;
-        border: none;
-        color: #007251;
-        padding: 10px 32px;
-        text-align: center;
-        text-decoration: none;
-        cursor: pointer;
-        margin-top: 40px;
-        width: 100%;
-    }
-</style>
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
-<div class="square1">
-    <div style="margin-top: 40px; margin-bottom: 25px;">
-        <p style="font-size: 50px; color: #fff; text-align: center;">
-            STULISH
-        </p>
-        <p style="font-size: 25px; color: #ccc; text-align: center;">
-            Study English for Kindergarten
-        </p>
-    </div>
+        <link rel="stylesheet" href="{{url('assets/admin/plugins/bootstrap/dist/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/plugins/fontawesome-free/css/all.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/plugins/icon-kit/dist/css/iconkit.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/plugins/ionicons/dist/css/ionicons.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/dist/css/theme.min.css')}}">
+        <link rel="stylesheet" href="{{url('assets/admin/src/js/vendor/modernizr-2.8.3.min.js')}}">
+    </head>
 
-    <p style="font-size: 15px; color: #fff; text-align: left;">
-        Login
-    </p>
+    <body>
 
-    @if (session('status'))
-    <div class="mb-4 font-medium text-sm text-green-600">
-        {{ session('status') }}
-    </div>
-    @endif
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div>
-            <label for="email" value="{{ __('Email') }}"></label>
-            <input class="input" id="email" type="email" name="email" :value="old('email')" placeholder="Email" required autofocus />
+        <div class="auth-wrapper">
+            <div class="container-fluid h-100">
+                <div class="row flex-row h-100 bg-white">
+                    <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
+                        <div class="lavalite-bg" style="background-image: url('assets/admin/img/image.jpg')">
+                            <div class="lavalite-overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-7 my-auto p-0">
+                        <div class="authentication-form mx-auto">
+                            <div class="logo-centered">
+                                <a href="../index.html"><img src="../src/img/brand.svg" alt=""></a>
+                            </div>
+                            <h3>Sign In to Stulish</h3>
+                            <p>Study English for Kindergarten</p>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input name="email" type="text" class="form-control" placeholder="Email" required="">
+                                    <i class="ik ik-user"></i>
+                                </div>
+                                <div class="form-group">
+                                    <input name="password" type="password" class="form-control" placeholder="Password" required="">
+                                    <i class="ik ik-lock"></i>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-right">
+                                        @if (Route::has('password.request'))
+                                        <a href="{{ route('password.request') }}">
+                                            {{ __('Forgot your password?') }}
+                                        </a>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="sign-btn text-center">
+                                    <button class="btn btn-theme">Sign In</button>
+                                </div>
+                            </form>
+                            <div class="register">
+                                <p>Don't have an account? <a href="{{route('register')}}">Create an account</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div>
-            <label for="password" value="{{ __('Password') }}"></label>
-            <input class="input" id="password" type="password" name="password" placeholder="Password" required autocomplete="current-password" />
-        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="assets/admin/src/js/vendor/jquery-3.3.1.min.js"><\/script>')
 
-        <div style="color:#fff">
-            <div style="float: left;">
-                <label for="remember_me">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span>{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    </script>
+    <script src="{{url('assets/admin/plugins/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{url('assets/admin/plugins/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{url('assets/admin/plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{url('assets/admin/plugins/screenfull/dist/screenfull.js')}}"></script>
+    <script src="{{url('assets/admin/dist/js/theme.min.js')}}"></script>
 
-            <div style="float: right;">
-                @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-                @endif
-            </div>
-
-        </div>
-        <div>
-            <button class="btn-login">{{ __('Log in') }}</button>
-        </div>
-
-    </form>
-
-</div>
-
-<!-- <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout> -->
+    </body>
+</html>
