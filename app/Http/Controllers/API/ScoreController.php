64a757b410161b63e4fd96248a5c0f09f615a546
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-12-08 15:40:22 +0700

    update score end point api

--- app/Http/Controllers/API/ScoreController.php
+++ app/Http/Controllers/API/ScoreController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ScoreResource;
 use App\Models\Score;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ScoreController extends Controller
 {
     public function getAllScore(Request $request)
     {
-        $data = Score::leftJoin('sub_categories', 'scores.sub_category_id', '=', 'sub_categories.id')
+        $data = SubCategory::join('scores', 'sub_categories.id', '=', 'scores.sub_category_id')
             ->where('user_id', '=', $request->user_id)
             ->get();
         return ScoreResource::collection($data);
